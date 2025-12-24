f800e060916aef1349fe833c8977e7e9d1e91256
kyle.chang <kyle.chang.cfk@gmail.com>
2025-12-25 00:15:20 +0800

    feat: create the main header component

--- src/shared/components/layouts/main-layout/MainLayout.component.tsx
+++ src/shared/components/layouts/main-layout/MainLayout.component.tsx
@@ -1,13 +1,21 @@
 import { Outlet } from 'react-router-dom';
 
 import AsideMenu from '../../aside-menu/AsideMenu.component';
+import MainHeader from '../../main-header/MainHeader.component';
+
+import { MainLayoutProvider } from './context/MainLayout.provider';
 
 const MainLayout = () => {
 	return (
-		<div className="flex h-screen bg-slate-50 overflow-hidden">
-			<AsideMenu></AsideMenu>
-			<Outlet></Outlet>
-		</div>
+		<MainLayoutProvider>
+			<div className="flex h-screen bg-slate-50 overflow-hidden">
+				<AsideMenu></AsideMenu>
+				<main className="flex-1 flex flex-col overflow-hidden min-w-0">
+					<MainHeader></MainHeader>
+					<Outlet></Outlet>
+				</main>
+			</div>
+		</MainLayoutProvider>
 	);
 };
 
