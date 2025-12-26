642de578f0c35e556d0a3b58e164595409098891
kyle <kyle.chang@ackuretta.com>
2025-12-26 18:37:52 +0800

    feat: add the mobile menu open controller to the MainLayout.provider

--- src/core/interfaces/main-layout-context-val.interface.ts
+++ src/core/interfaces/main-layout-context-val.interface.ts
@@ -1,4 +1,7 @@
 export interface MainLayoutContextVal {
 	activeNav: string;
 	setActiveNav: React.Dispatch<React.SetStateAction<string>>;
+
+	isMobileMenuOpen: boolean;
+	setIsMobileMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
