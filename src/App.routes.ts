ebb480de49518fc35a345e7f1e3b1d0e0efbb779
kyle <kyle.chang@ackuretta.com>
2025-12-26 18:56:29 +0800

    feat: update the routes to match all the pages

--- src/App.routes.ts
+++ src/App.routes.ts
@@ -21,6 +21,55 @@ export const router = createHashRouter([
 					return { Component: DashboardPage };
 				},
 			},
+			{
+				path: ROUTES.profile,
+				lazy: async () => {
+					const { default: ProfilePage } = await import('./pages/profile/Profile.page');
+					return { Component: ProfilePage };
+				},
+			},
+			{
+				path: ROUTES.careerPaths,
+				lazy: async () => {
+					const { default: CareerPage } = await import('./pages/career-path/CareerPath.page');
+					return { Component: CareerPage };
+				},
+			},
+			{
+				path: ROUTES.education,
+				lazy: async () => {
+					const { default: EducationPage } = await import('./pages/education/Education.page');
+					return { Component: EducationPage };
+				},
+			},
+			{
+				path: ROUTES.experience,
+				lazy: async () => {
+					const { default: ExperiencePage } = await import('./pages/experience/Experience.page');
+					return { Component: ExperiencePage };
+				},
+			},
+			{
+				path: ROUTES.projects,
+				lazy: async () => {
+					const { default: ProjectsPage } = await import('./pages/projects/Projects.page');
+					return { Component: ProjectsPage };
+				},
+			},
+			{
+				path: ROUTES.skills,
+				lazy: async () => {
+					const { default: SkillsPage } = await import('./pages/skills/Skills.page');
+					return { Component: SkillsPage };
+				},
+			},
+			{
+				path: ROUTES.statistics,
+				lazy: async () => {
+					const { default: Statistics } = await import('./pages/statistics/Statistics.page');
+					return { Component: Statistics };
+				},
+			},
 		],
 	},
 ]);
