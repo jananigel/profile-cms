1996898ac4813d1a3e2a1c8209e8d8b8cb822259
kyle <kyle.chang@ackuretta.com>
2025-12-29 16:57:07 +0800

    feat: add the stat cards to the dashboard page

--- src/pages/dashboard/Dashboard.page.tsx
+++ src/pages/dashboard/Dashboard.page.tsx
@@ -1,5 +1,16 @@
+import { DASHBOARD_STATS } from '../../core/constants';
+import DashboardStatCard from '../../shared/components/dashboard-stat-card/dashboard-stat-card.component';
+
 const DashboardPage = () => {
-	return <>Dashboard</>;
+	return (
+		<div className="space-y-8">
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+				{DASHBOARD_STATS.map((stat, index) => {
+					return <DashboardStatCard key={index} data={stat}></DashboardStatCard>;
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default DashboardPage;
