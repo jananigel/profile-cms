2c485b7c3f7956e3f2b503e5a598e9e1be1b88de
kyle <kyle.chang@ackuretta.com>
2025-12-29 19:54:44 +0800

    feat: create the completeness card component

--- src/pages/dashboard/Dashboard.page.tsx
+++ src/pages/dashboard/Dashboard.page.tsx
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import DashboardStatCard from '../../shared/components/dashboard-stat-card/dashboard-stat-card.component';
 
 import { selectDashboardStats } from './dashboard.selector';
+import CompletenessCard from './shared/components/CompletenessCard.component';
 
 import type { RootState } from '../../stores/store';
 const DashboardPage = () => {
@@ -13,6 +14,9 @@ const DashboardPage = () => {
 				{stats.map((stat, index) => {
 					return <DashboardStatCard key={index} data={stat}></DashboardStatCard>;
 				})}
+			</div>
+			<div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+				<CompletenessCard></CompletenessCard>
 			</div>
 		</div>
 	);
