5393807e1dcaec0f91a7672136cedc1414237dbc
kyle <kyle.chang@ackuretta.com>
2026-01-02 03:07:00 +0800

    feat: create the BaseCheckbox component

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,3 +1,5 @@
 export * from './layouts/card-layout/CardLayout.component';
 export * from './animated-arc-progress/AnimatedArcProgress.component';
 export * from './animated-circular-progress/AnimatedCircularProgress.component';
+export * from './base-input/BaseInput.component';
+export * from './base-checkbox/BaseCheckbox.compnent';
