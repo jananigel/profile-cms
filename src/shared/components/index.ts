b658a3fa4e07bba31af85a0fe37185354d0ec35d
kyle <kyle.chang@ackuretta.com>
2026-01-04 01:07:42 +0800

    feat: create the base select component

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -4,3 +4,4 @@ export * from './animated-circular-progress/AnimatedCircularProgress.component';
 export * from './base-input/BaseInput.component';
 export * from './base-checkbox/BaseCheckbox.compnent';
 export * from './base-textarea/BaseTextarea.component';
+export * from './base-select/BaseSelect.component';
