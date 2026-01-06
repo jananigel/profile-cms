1d2492e884b717008db4e6005cabe8198845b066
kyle.chang <kyle.chang.cfk@gmail.com>
2026-01-06 23:40:09 +0800

    feat: add skill editor to the skill page

--- src/pages/skills/Skills.page.tsx
+++ src/pages/skills/Skills.page.tsx
@@ -1,5 +1,7 @@
 import PageHeader from '../../shared/components/page-header/PageHeader.component';
 
+import SkillEditor from './SkillEdittor.component';
+
 const SkillsPage = () => {
 	return (
 		<div className="space-y-8">
@@ -7,6 +9,7 @@ const SkillsPage = () => {
 				title="Skills & Competencies"
 				description="Your technical stack categorized with proficiency details."
 			/>
+			<SkillEditor />
 		</div>
 	);
 };
