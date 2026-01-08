36ffd1c04f4b27f53dd214d707bb531bdec701cb
kyle <kyle.chang@ackuretta.com>
2026-01-08 11:57:38 +0800

    feat: create the page header for the statistics page

--- src/pages/statistics/Statistics.page.tsx
+++ src/pages/statistics/Statistics.page.tsx
@@ -1,5 +1,14 @@
+import PageHeader from '../../shared/components/page-header/PageHeader.component';
+
 const StatisticsPage = () => {
-	return <>statistics</>;
+	return (
+		<div className="space-y-6">
+			<PageHeader
+				title="Statistics"
+				description="Quantifiable achievements for your landing page."
+			/>
+		</div>
+	);
 };
 
 export default StatisticsPage;
