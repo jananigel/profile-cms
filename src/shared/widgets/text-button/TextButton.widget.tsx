9ade3a9a2cb4bd98c7bae505b13965b7b13aa3ca
kyle <kyle.chang@ackuretta.com>
2026-01-20 17:28:38 +0800

    feat: add new button style of indigo to the TextButton widget

--- src/shared/widgets/text-button/TextButton.widget.tsx
+++ src/shared/widgets/text-button/TextButton.widget.tsx
@@ -6,7 +6,7 @@ interface TextButtonProps {
 	label: string;
 	size?: 'auto' | 'full' | 'xl' | 'lg' | 'md' | 'sm' | 'xs';
 	btnType?: 'submit' | 'reset' | 'button';
-	btnStyle?: 'primary' | 'secondary' | 'amber' | 'emerald';
+	btnStyle?: 'primary' | 'secondary' | 'amber' | 'emerald' | 'indigo';
 	callback?: MouseEventHandler<HTMLButtonElement>;
 	isDisabled?: boolean;
 	isLoading?: boolean;
@@ -36,6 +36,8 @@ const TextButton = ({
 		amber:
 			'w-full py-3 text-white rounded-xl font-bold transition-all shadow-lg disabled:opacity-50 bg-amber-500 hover:bg-amber-600 shadow-amber-100',
 		emerald: 'bg-emerald-600 hover:bg-emerald-700 shadow-emerald-100 text-white ',
+		indigo:
+			'w-full py-3 text-white rounded-xl font-bold transition-all shadow-lg disabled:opacity-50 bg-indigo-600 hover:bg-indigo-700 shadow-indigo-100',
 	} as const;
 
 	const getBtnStyle = (): string => {
